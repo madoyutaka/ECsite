9a9beff8ac1277983e352d43fe590a52e303c84e
GWMonma <83321035+GWMonma@users.noreply.github.com>
2021-04-30 17:14:45 +0900

    セッション管理追加

--- ECSite/src/signup/SignUpServlet.java
+++ ECSite/src/signup/SignUpServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import bean.UserBean;
 import jdbc.UserJdbc;
@@ -24,6 +25,28 @@ public class SignUpServlet extends HttpServlet {
 			response.setContentType("text/html;charset=UTF-8");
 			//requestのエンコーディング
 			request.setCharacterEncoding("UTF-8");
+
+			RequestDispatcher req = null;
+			HttpSession session = request.getSession(false);
+			UserBean loginUserSession;
+			try {
+				loginUserSession = (UserBean)session.getAttribute("loginUser");
+			}catch(Exception ex) {
+				loginUserSession = null;
+			}
+		//セッションが継続している場合はマイページへ
+			if(loginUserSession != null) {
+				//セッションから値を取得
+				UserBean loginUserBean = (UserBean) session.getAttribute("loginUser");
+				//request.setAttribute("userData", loginUserBean);
+				request.setAttribute("loginUser", loginUserBean);//パラメータ名はloginUser
+				//画面遷移
+				req = request.getRequestDispatcher("jsp/MyPage.jsp");
+				req.forward(request, response);
+				return;
+			}
+
+
 
 
 			//値の受け取りと格納
@@ -60,16 +83,16 @@ public class SignUpServlet extends HttpServlet {
 					request.setAttribute("message", "メールアドレスに不正な文字が入力されています");
 					request.getRequestDispatcher("/jsp/SignUp.jsp").forward(request, response);
 					return;
-				}else if(pCode.length()!=7||!pCode.matches("^[0-9]+$)")){
+				}else if(pCode.length()!=7||!pCode.matches("^[0-9]+$")){
 					request.setAttribute("message", "郵便番号はハイフンなし半角数字7桁で入力してください");
 					request.getRequestDispatcher("/jsp/SignUp.jsp").forward(request, response);
 					return;
 				}else {
 
-				//beanのインスタンス作成
-				UserBean ub = new UserBean();
 				//pCodeをint型に変換しそれぞれbeanにセット
 				int postalCode = Integer.parseInt(pCode);
+
+				UserBean ub = new UserBean();
 				ub.setUserId(userId);
 				ub.setPassword(password);
 				ub.setEmailAddress(emailAddress);
@@ -77,23 +100,24 @@ public class SignUpServlet extends HttpServlet {
 				ub.setAddress(address);
 				ub.setUserName(userName);
 
-	//			SignUpLogic newlogic = new SignUpLogic();
-	//			String resultText = newlogic.SULogic(userId,password,emailAddress,postalCode,address,userName);
-
 				UserJdbc uj = new UserJdbc();
 				uj.insert(ub);
 
-				//画面遷移
-				RequestDispatcher rd = request.getRequestDispatcher("/jsp/MyPage.jsp");
-				rd.forward(request, response);
+				if(ub != null){
+		    		// セッションにアカウント情報
+			    	session = request.getSession();
+		            session.setAttribute("loginUser", ub);
+		            System.out.println("session start");
+					request.setAttribute("loginUser",ub);//パラメータ名はloginUser
+					//Mypage.jspに画面遷移
+					RequestDispatcher rd = request.getRequestDispatcher("/jsp/MyPage.jsp");
+					rd.forward(request, response);
+					return;
+
 				}
-
-
-		}
+			}
 	}
-}
-
-
+}}
 
 
 
