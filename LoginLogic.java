ff77c0a561f997bfa22f21f6dcc4ac4b7536c26f
yutaka <40336504+madoyutaka@users.noreply.github.com>
2021-04-30 16:21:27 +0900

    ログイン部分の修正
    ロジック追加

---
+++ LoginLogic.java
@@ -0,0 +1,21 @@
+package login;
+
+import bean.UserBean;
+import jdbc.UserJdbc;
+
+public class LoginLogic {
+	public UserBean accountLoginLogic(String UserId, String UserPass) {
+
+		//	returnする文章を入れる
+		UserJdbc jdbc = new UserJdbc();
+		UserBean returnUser = null;
+
+
+		returnUser  = jdbc.LoginAccount(UserId,UserPass);
+
+		System.out.println(returnUser+"を返します。");
+		return returnUser;
+
+	}
+}
+
