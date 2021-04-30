2e9ba0111b71cdfd4c4043c27bb26eec3d4105f3
GWMonma <83321035+GWMonma@users.noreply.github.com>
2021-04-30 14:40:06 +0900

    Add files via upload

--- ECSite/src/SignUp/SignUpServlet.java
+++ ECSite/src/SignUp/SignUpServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import bean.UserBean;
 import jdbc.UserJdbc;
@@ -17,27 +16,19 @@ import jdbc.UserJdbc;
 public class SignUpServlet extends HttpServlet {
 
 
-	//セッション管理
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		HttpSession session = request.getSession(false);
-		if(session==null) {
-			System.out.println("セッションが開始していません");
-			RequestDispatcher req = request.getRequestDispatcher("jsp/Login.jsp");
-			req.forward(request, response);
-		}
-		System.out.println("セッション継続中");
-	}
-
-
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		//MIMEタイプとエンコーディング(文字コード)の設定
+		response.setContentType("text/html;charset=UTF-8");
+		//requestのエンコーディング
 		request.setCharacterEncoding("UTF-8");
 
 
+
+
+		//値の受け取りと格納
 		String userId = request.getParameter("userId");
 		String password = request.getParameter("password");
 		String emailAddress = request.getParameter("emailAddress");
@@ -45,51 +36,66 @@ public class SignUpServlet extends HttpServlet {
 		String address = request.getParameter("address");
 		String userName = request.getParameter("userName");
 
+		//フォームでの値の保持用
+		request.setAttribute("id", userId);
+		request.setAttribute("pass", password);
+		request.setAttribute("mail", emailAddress);
+		request.setAttribute("pcode", pCode);
+		request.setAttribute("add", address);
+		request.setAttribute("name", userName);
+
+		//入力チェック
 		if(userId=="" || password=="" || emailAddress=="" || pCode=="" || address=="" || userName=="") {
 			request.setAttribute("message", "未入力の項目があります");
 			request.getRequestDispatcher("/jsp/SignUp.jsp").forward(request, response);
 			return;
+		}else {
 
-		}else if(pCode.length()!=7||pCode.matches("^[^\\x01-\\x7E]")||pCode.matches("^[a-zA-Z]+$")){
-			request.setAttribute("message", "郵便番号はハイフンなし半角数字7桁で入力してください");
-			request.getRequestDispatcher("/jsp/SignUp.jsp").forward(request, response);
-			return;
+			if(userId.length()>=20||password.length()>=20||emailAddress.length()>=40||address.length()>=100||userName.length()>=20){
+				request.setAttribute("message", "文字数オーバーです");
+				request.getRequestDispatcher("/jsp/SignUp.jsp").forward(request, response);
+				return;
 
-		}else if(userId.length()>=20||password.length()>=20||emailAddress.length()>=40||address.length()>=20||userName.length()>=100){
-			request.setAttribute("message", "文字数オーバーです");
-			request.getRequestDispatcher("/jsp/SignUp.jsp").forward(request, response);
-			return;
+			}else if(userId.matches("^[^\\x01-\\x7E]")||password.matches("^[^\\x01-\\x7E]")||emailAddress.matches("^[^\\x01-\\x7E]")){
+				request.setAttribute("message", "ID,メールアドレス、パスワードは半角英数字のみ入力してください");
+				request.getRequestDispatcher("/jsp/SignUp.jsp").forward(request, response);
+				return;
+			}else if(pCode.length()!=7||pCode.matches("^[^\\x01-\\x7E]")||pCode.matches("^[a-zA-Z]+$")){
+				request.setAttribute("message", "郵便番号はハイフンなし半角数字7桁で入力してください");
+				request.getRequestDispatcher("/jsp/SignUp.jsp").forward(request, response);
+				return;
+			}else {
+				UserBean ub = new UserBean();
+
+				//pCodeをint型に変換しそれぞれbeanにセット
+				int postalCode = Integer.parseInt(pCode);
+				ub.setUserId(userId);
+				ub.setPassword(password);
+				ub.setEmailAddress(emailAddress);
+				ub.setPostalCode(postalCode);
+				ub.setAddress(address);
+				ub.setUserName(userName);
+
+				UserJdbc uj = new UserJdbc();
+				uj.insert(ub);
+
+
+				//画面遷移
+				RequestDispatcher rd = request.getRequestDispatcher("/jsp/MyPage.jsp");
+				rd.forward(request, response);
+			}
 
 		}
-		else if(userId.matches("^[^\\x01-\\x7E]")||password.matches("^[^\\x01-\\x7E]")||emailAddress.matches("^[^\\x01-\\x7E]")){
-			request.setAttribute("message", "ID,メールアドレス、パスワードは半角英数字のみ入力してください");
-			request.getRequestDispatcher("/jsp/SignUp.jsp").forward(request, response);
-			return;
 		}
-		else {
-			int postalCode = Integer.parseInt(pCode);
-			UserBean ub = new UserBean();
-			
-			ub.setUserId(userId);
-			ub.setPassword(password);
-			ub.setEmailAddress(emailAddress);
-			ub.setPostalCode(postalCode);
-			ub.setAddress(address);
-			ub.setUserName(userName);
-
-			UserJdbc uj = new UserJdbc();
-			uj.insert(ub);
-
-			RequestDispatcher rd = request.getRequestDispatcher("/jsp/MyPage.jsp");
-			rd.forward(request, response);
-		}
-
-
-		}
-
-
-
-
-
 	}
+
+
+
+
+
+
+
+
+
+
 
