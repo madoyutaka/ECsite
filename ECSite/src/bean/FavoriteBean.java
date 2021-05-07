5dc391baca26ad0f94761d89cc6c287eed4bbb2e
GWMonma <83321035+GWMonma@users.noreply.github.com>
2021-05-07 15:47:03 +0900

    お気に入りBean追加

---
+++ ECSite/src/bean/FavoriteBean.java
@@ -0,0 +1,43 @@
+package bean;
+
+public class FavoriteBean {
+
+
+	String itemName=null;
+	int userNo;
+	int favoriteNo;
+	String itemImage =null;
+
+
+
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+}
+
+	public int getFavoriteNo() {
+		return favoriteNo;
+	}
+	public void setFavoriteNo(int favoriteNo) {
+		this.favoriteNo = favoriteNo;
+	}
+
+
+
+	public String getItemImage() {
+		return itemImage;
+	}
+	public void setItemImage(String itemImage) {
+		this.itemImage = itemImage;
+	}
+
+}
