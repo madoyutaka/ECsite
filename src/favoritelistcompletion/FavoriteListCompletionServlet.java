3ae9001e3239f5214d4ec9fdccf60765ad35ee75
GWMonma <83321035+GWMonma@users.noreply.github.com>
2021-05-10 15:54:35 +0900

    Add files via upload

--- src/favoritelistcompletion/FavoriteListCompletionServlet.java
+++ src/favoritelistcompletion/FavoriteListCompletionServlet.java
@@ -1,6 +1,7 @@
 package favoritelistcompletion;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,7 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.FavoriteBean;
 import bean.UserBean;
+import favoritelist.FavoriteListLogic;
 
 
 @WebServlet("/FavoriteListCompletionServlet")
@@ -23,22 +26,23 @@ public class FavoriteListCompletionServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-
-		//MIMタイプとエンコーディング(文字コード)の設定をする
+				//MIMタイプとエンコーディング(文字コード)の設定をする
 				response.setContentType("text/html;charset=UTF-8");
 			//requestで送られてきたパラメータのエンコーディングを設定する
 				request.setCharacterEncoding("UTF-8");
-			//保存用
+
+				//保存用
 				RequestDispatcher req = null;
-				HttpSession session = request.getSession(false);
+				int loginUserNo;
+				HttpSession session = request.getSession();
+				UserBean loginUserBean = (UserBean) session.getAttribute("loginUser");
 				UserBean loginUserSession;
 				try {
 					loginUserSession = (UserBean)session.getAttribute("loginUser");
 				}catch(Exception ex) {
 					loginUserSession = null;
 				}
-
-			//セッションが継続していなかったときは処理を行わずにログイン画面へ
+				//	セッションが継続していなかったときは処理を行わずにログイン画面へ
 				if(loginUserSession == null) {
 					System.out.println("セッションが開始していません。");
 					//画面遷移
@@ -47,10 +51,39 @@ public class FavoriteListCompletionServlet extends HttpServlet {
 					return;
 				}
 
+				//セッションから値を取得
+				loginUserNo = loginUserBean.getUserNo();
+
+				//インスタンスを生成し、処理を行った結果を格納する。
+				FavoriteListLogic  faveLogic = new FavoriteListLogic();
+				ArrayList<FavoriteBean> faveList = faveLogic.favoriteGetLogic(loginUserNo);
+
+
 				if(request.getParameter("btnFavoriteListTransition")!=null) {
 
 					System.out.println("お気に入りリスト画面に遷移します。");
 					req = request.getRequestDispatcher("jsp/FavoriteList.jsp");
+					req.forward(request, response);
+					return;
+				}
+
+				if(request.getParameter("btnFavoriteListDeleteTransition")!=null) {
+					System.out.println("favoriteNo："+request.getParameter("btnFavoriteListDeleteTransition")+"を削除");
+
+					int favoriteNo = Integer.parseInt(request.getParameter("btnFavoriteListDeleteTransition"));
+					FavoriteBean faveBean = new FavoriteBean();
+					faveBean = faveLogic.deleteLogic(favoriteNo);
+					request.setAttribute("message", "お気に入りリストから商品を削除しました。");
+					req = request.getRequestDispatcher("jsp/FavoriteCompletionList.jsp");
+					req.forward(request, response);
+					return;
+				}
+
+				if(request.getParameter("btnFavoriteListAddTransition")!=null) {
+
+					System.out.println("お気に入りリストに商品を追加しました。");
+					request.setAttribute("message", "お気に入りリストに商品を追加しました。");
+					req = request.getRequestDispatcher("jsp/FavoriteCompletionList.jsp");
 					req.forward(request, response);
 					return;
 				}
