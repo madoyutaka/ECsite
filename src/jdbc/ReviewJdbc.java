0081d0e2edf46ab90538d7aae505041932d81860
OkajimaAyaka <83321115+OkajimaAyaka@users.noreply.github.com>
2021-05-11 10:12:18 +0900

    ReviewJdbc.java
    ReviewJdbc.javaの追加。

--- src/jdbc/ReviewJdbc.java
+++ src/jdbc/ReviewJdbc.java
@@ -6,11 +6,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+
+import bean.ReviewBean;
 
 public class ReviewJdbc {
 	//returnする文章を入れる
 		String returnText = null;
 		String query = null;
+		ArrayList<ReviewBean> reviewList = new ArrayList<ReviewBean>();
 
 		Connection conn = null;
 		Statement stmt = null;
@@ -21,7 +25,56 @@ public class ReviewJdbc {
 		String id = "root";
 		String pw = "1qaz2wSX?";
 
-	//レビューを保存
+		//レビューを取得する
+		public ArrayList<ReviewBean> getReviewData(int setItemNo) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				conn = DriverManager.getConnection(url, id, pw);
+				stmt =conn.createStatement();
+				//レビューを新しい順で取得する。
+				query = "SELECT * FROM review WHERE item_no=? ORDER BY review_no DESC";
+
+				//PreparedStatementオブジェクトを使用
+				pstmt = conn.prepareStatement(query);
+				pstmt.setInt(1, setItemNo);
+				//SQLの実行
+				rs = pstmt.executeQuery();
+
+				//取得した値を格納
+				while(rs.next()) {
+					ReviewBean reviewBean = new ReviewBean();
+					reviewBean.setReviewComment(rs.getString("review_comment"));
+					reviewBean.setReviewScore(rs.getInt("review_score"));
+					reviewBean.setItemNo(rs.getInt("item_no"));
+					reviewList.add(reviewBean);
+					System.out.println(reviewBean+"を追加しました。");
+				}
+
+			}catch(SQLException ex) {
+				ex.printStackTrace();
+
+			}catch(Exception ex) {
+				ex.printStackTrace();
+
+			}finally {
+				try {
+					if(conn != null) { conn.close(); }
+					if(stmt != null) { stmt.close(); }
+					if(pstmt != null) { pstmt.close(); }
+					if(rs != null) { rs.close(); }
+
+				}catch(SQLException ex){
+					ex.printStackTrace();
+
+					}
+
+			}
+
+			return reviewList;
+		}
+
+
+		//レビューを保存
 		public String writeReview(int loginUserNo, int setItemNo, double setReviewScore, String setReviewComment) {
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
@@ -29,7 +82,6 @@ public class ReviewJdbc {
 				stmt =conn.createStatement();
 
 				System.out.println(loginUserNo+"が入力したレビュー「"+setReviewScore+"、"+setReviewComment+"」を保存します。");
-				//IDとメールアドレスは値が重複していないか確認する。
 				query = "INSERT INTO review(review_comment, review_score, item_no, user_no) VALUE(?, ?, ?, ?)";
 
 				//PreparedStatementオブジェクトを使用
