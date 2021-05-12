c4cfec2e63cafd7da0ed09e61893f90f9ee9b008
OkajimaAyaka <83321115+OkajimaAyaka@users.noreply.github.com>
2021-05-12 11:30:29 +0900

    CartBean.javaの追加。
    CartBean.javaの追加を行いました。

---
+++ src/bean/CartBean.java
@@ -0,0 +1,22 @@
+package bean;
+
+public class CartBean {
+
+	int itemNo;
+	int itemBuyCount;
+
+	public int getItemNo() {
+		return itemNo;
+	}
+	public void setItemNo(int itemNo) {
+		this.itemNo = itemNo;
+	}
+
+	public int getItemBuyCount() {
+		return itemBuyCount;
+	}
+	public void setItemBuyCount(int itemBuyCount) {
+		this.itemBuyCount = itemBuyCount;
+	}
+
+}
