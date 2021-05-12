7cd229db8bba50bbc698309543a89654e6b3ee81
OkajimaAyaka <83321115+OkajimaAyaka@users.noreply.github.com>
2021-05-12 15:23:51 +0900

    ItemBuyCompletionServlet.javaの編集。
    ItemBuyCompletionServlet.javaの編集を行いました。

--- src/itembuycompletion/ItemBuyCompletionServlet.java
+++ src/itembuycompletion/ItemBuyCompletionServlet.java
@@ -1,6 +1,7 @@
 package itembuycompletion;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,8 +11,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.CartBean;
 import bean.ItemBean;
 import bean.UserBean;
+import cart.CartLogic;
+import itembuy.ItemBuyLogic;
 
 @WebServlet("/ItemBuyCompletionServlet")
 public class ItemBuyCompletionServlet extends HttpServlet {
@@ -27,43 +31,58 @@ public class ItemBuyCompletionServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			//MIMタイプとエンコーディング(文字コード)の設定をする
-				response.setContentType("text/html;charset=UTF-8");
-			//requestで送られてきたパラメータのエンコーディングを設定する
-				request.setCharacterEncoding("UTF-8");
-			//保存用
-				RequestDispatcher req = null;
-				HttpSession session = request.getSession();
-				UserBean loginUserSession;
-				ItemBean itemBean = new ItemBean();
-				try {
-					loginUserSession = (UserBean)session.getAttribute("loginUser");
-				}catch(Exception ex) {
-					loginUserSession = null;
-				}
-				//	セッションが継続していなかったときは処理を行わずにログイン画面へ
-				if(loginUserSession == null) {
-					System.out.println("セッションが開始していません。");
-					//画面遷移
-					req = request.getRequestDispatcher("jsp/Login.jsp");
-					req.forward(request, response);
-					return;
-				}
+		//requestで送られてきたパラメータのエンコーディングを設定する
+		request.setCharacterEncoding("UTF-8");
+		//保存用
+		RequestDispatcher req = null;
+		CartLogic newLogic = new CartLogic();
+		HttpSession session = request.getSession(false);
+		UserBean loginUserBean = (UserBean) session.getAttribute("loginUser");
+		int loginUserNo = loginUserBean.getUserNo();
+		UserBean loginUserSession;
+		try {
+			loginUserSession = (UserBean)session.getAttribute("loginUser");
+		}catch(Exception ex) {
+			loginUserSession = null;
+		}
+		//	セッションが継続していなかったときは処理を行わずにログイン画面へ
+		if(loginUserSession == null) {
+			System.out.println("セッションが開始していません。");
+			//画面遷移
+			req = request.getRequestDispatcher("jsp/Login.jsp");
+			req.forward(request, response);
+			return;
+		}
+		ArrayList<CartBean> loginItemSession= (ArrayList<CartBean>) session.getAttribute("CartItem");
+		ArrayList<ItemBean> ItemBeanList = newLogic.getCartItemData(loginItemSession);
 
-				//購入完了画面に遷移する。
-				if(request.getParameter("btnItemBuyCompletionTransition")!=null) {
-					//変更するかも
-					request.setAttribute("itemData", itemBean);
-					//画面遷移
-					System.out.println("購入完了画面に遷移します。");
-					req = request.getRequestDispatcher("jsp/ItemBuyCompletion.jsp");
-					req.forward(request, response);
-					return;
-				}
+		//購入処理を行う。
+		if(request.getParameter("btnItemBuyCompletionTransition")!=null) {
 
-				//カートの中身を空にする
+			//インスタンスを生成し、処理を行った結果を格納する。
+			ItemBuyLogic itemBuyLogic = new ItemBuyLogic();
+			String itemBuyResultText = itemBuyLogic.itemBuy(loginItemSession, ItemBeanList, loginUserNo);
+			//表示用のデータを取得し、パラメータ名set
+			request.setAttribute("itemBuyResultText", itemBuyResultText);
 
-				//購入履歴へのデータ受け渡し
+			//画面遷移if文
+			if(itemBuyResultText.equals("")) {
+				//中身をにする。
+				session.setAttribute("CartItem",new ArrayList<CartBean>());
+				System.out.println("購入処理完了画面に遷移します。");
+				req = request.getRequestDispatcher("jsp/ItemBuyCompletion.jsp");
+				req.forward(request, response);
+				return;
+			}else {
+				//中身変更無し
+				session.setAttribute("CartItem",loginItemSession);
+				System.out.println("購入処理完了画面に遷移します。");
+				req = request.getRequestDispatcher("jsp/ItemBuyCompletion.jsp");
+				req.forward(request, response);
+				return;
+			}
+		}
+
 	}
 
 }
