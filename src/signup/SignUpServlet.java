2aabae892838c038ac4e7f15fe8a704f2ad64baf
GWMonma <83321035+GWMonma@users.noreply.github.com>
2021-05-12 14:19:56 +0900

    SignUpServlet　エラーメッセージ変更

--- src/signup/SignUpServlet.java
+++ src/signup/SignUpServlet.java
@@ -1,6 +1,7 @@
 package signup;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -15,15 +16,13 @@ import jdbc.UserJdbc;
 @WebServlet("/SignUpServlet")
 public class SignUpServlet extends HttpServlet {
 
-
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		//MIMEタイプとエンコーディング(文字コード)の設定
-		response.setContentType("text/html;charset=UTF-8");
 		//requestのエンコーディング
 		request.setCharacterEncoding("UTF-8");
+
+		//HttpSession session = request.getSession(false);
+
 
 		if(request.getParameter("btnSignUpTransition")!=null) {
 			//画面遷移
@@ -51,49 +50,52 @@ public class SignUpServlet extends HttpServlet {
 		request.setAttribute("name", userName);
 
 		//入力チェック
+			//エラー用リスト
+		ArrayList<String> list = new ArrayList<String>();
+
 		if(userId=="" || password=="" || emailAddress=="" || pCode=="" || address=="" || userName=="") {
-			request.setAttribute("message", "未入力の項目があります");
-			request.getRequestDispatcher("/jsp/SignUp.jsp").forward(request, response);
-			return;
+			list.add("!入力されていない項目があります");
 		}else {
-
-			if(userId.length()>=20||password.length()>=20||emailAddress.length()>=40||address.length()>=100||userName.length()>=20){
-				request.setAttribute("message", "文字数オーバーです");
-				request.getRequestDispatcher("/jsp/SignUp.jsp").forward(request, response);
-				return;
-
-			}else if(userId.matches("^[^\\x01-\\x7E]")||password.matches("^[^\\x01-\\x7E]")||emailAddress.matches("^[^\\x01-\\x7E]")){
-				request.setAttribute("message", "ID,メールアドレス、パスワードは半角英数字のみ入力してください");
-				request.getRequestDispatcher("/jsp/SignUp.jsp").forward(request, response);
-				return;
-			}else if(pCode.length()!=7||pCode.matches("^[^\\x01-\\x7E]")||pCode.matches("^[a-zA-Z]+$")){
-				request.setAttribute("message", "郵便番号はハイフンなし半角数字7桁で入力してください");
-				request.getRequestDispatcher("/jsp/SignUp.jsp").forward(request, response);
-				return;
-			}else {
-				UserBean ub = new UserBean();
-
-				//pCodeをint型に変換しそれぞれbeanにセット
-				int postalCode = Integer.parseInt(pCode);
-				ub.setUserId(userId);
-				ub.setPassword(password);
-				ub.setEmailAddress(emailAddress);
-				ub.setPostalCode(postalCode);
-				ub.setAddress(address);
-				ub.setUserName(userName);
-
-				UserJdbc uj = new UserJdbc();
-				uj.insert(ub);
-
-
-				//画面遷移
-				RequestDispatcher rd = request.getRequestDispatcher("/jsp/MyPage.jsp");
-				rd.forward(request, response);
-			}
-
+		if(userId.length()>=20||password.length()>=20||emailAddress.length()>=40||address.length()>=100||userName.length()>=20){
+			list.add("!文字数オーバーの項目があります");
+		}
+		if(!emailAddress.matches("^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$")) {
+			list.add("!メールアドレスは正しい方法で入力してください");
+		}
+		if(!userId.matches("^[a-zA-Z0-9]+$")||!password.matches("^[a-zA-Z0-9]+$")){
+			list.add("!IDとパスワードは半角英数字で入力してください");
+		}
+		if(pCode.length()!=7||!pCode.matches("^[0-9]+$")){
+			list.add("!郵便番号はハイフンなし半角数字7文字で入力してください");
 		}
 		}
-	}
+
+		if(list.isEmpty()==false) {
+			request.setAttribute("list", list);
+			request.getRequestDispatcher("/jsp/SignUp.jsp").forward(request, response);
+		}else {
+			UserBean ub = new UserBean();
+			//pCodeをint型に変換しそれぞれbeanにセット
+			int postalCode = Integer.parseInt(pCode);
+			ub.setUserId(userId);
+			ub.setPassword(password);
+			ub.setEmailAddress(emailAddress);
+			ub.setPostalCode(postalCode);
+			ub.setAddress(address);
+			ub.setUserName(userName);
+
+			UserJdbc uj = new UserJdbc();
+			uj.insert(ub);
+
+			//画面遷移
+			RequestDispatcher rd = request.getRequestDispatcher("/jsp/MyPage.jsp");
+			rd.forward(request, response);
+		}
+
+
+		}
+}
+
 
 
 
