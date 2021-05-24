c58c2d1fda6ee05e111fc98ac738b74ef0fa426a
OkajimaAyaka <83321115+OkajimaAyaka@users.noreply.github.com>
2021-05-24 11:57:21 +0900

    TopServlet.javaの編集。
    ランダムで表示した商品のレビューの平均点が高い場合、特定のマークを表示する機能を追加しました。

--- src/top/TopServlet.java
+++ src/top/TopServlet.java
@@ -1,8 +1,8 @@
 package top;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,8 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import bean.ItemBean;
+import bean.ReviewBean;
 import bean.UserBean;
+import itemdetail.ItemDetailLogic;
 import jdbc.ItemJdbc;
+import jdbc.ReviewJdbc;
 
 @WebServlet("/TopServlet")
 public class TopServlet extends HttpServlet {
@@ -33,15 +36,23 @@ public class TopServlet extends HttpServlet {
 
 		ItemJdbc itemJdbc = new ItemJdbc();
 		ItemBean randomItemBean = itemJdbc.randomItem();
+		//レビュー一覧
+		ArrayList<ReviewBean> reviewList = new ArrayList<ReviewBean>();
+		ReviewJdbc reviewJdbc = new ReviewJdbc();
+		reviewList = reviewJdbc.getReviewData(randomItemBean.getItemNo());
+		//レビューの平均点を取得
+		ItemDetailLogic itemDetailLogic = new ItemDetailLogic();
+		double reviewAverage = itemDetailLogic.getReviewAverage(reviewList, randomItemBean.getItemNo());
 		//値を渡す
 		request.setAttribute("randomItem", randomItemBean);
+		request.setAttribute("reviewAverage", reviewAverage);
 		request.getRequestDispatcher("jsp/Top.jsp").forward(request, response);
+		return;
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//requestで送られてきたパラメータのエンコーディングを設定する
 		request.setCharacterEncoding("UTF-8");
-		RequestDispatcher req = null;
 		HttpSession session = request.getSession(false);
 		UserBean loginUserSession;
 		try {
@@ -60,8 +71,16 @@ public class TopServlet extends HttpServlet {
 
 				ItemJdbc itemJdbc = new ItemJdbc();
 				ItemBean randomItemBean = itemJdbc.randomItem();
+				//レビュー一覧
+				ArrayList<ReviewBean> reviewList = new ArrayList<ReviewBean>();
+				ReviewJdbc reviewJdbc = new ReviewJdbc();
+				reviewList = reviewJdbc.getReviewData(randomItemBean.getItemNo());
+				//レビューの平均点を取得
+				ItemDetailLogic itemDetailLogic = new ItemDetailLogic();
+				double reviewAverage = itemDetailLogic.getReviewAverage(reviewList, randomItemBean.getItemNo());
 				//値を渡す
 				request.setAttribute("randomItem", randomItemBean);
+				request.setAttribute("reviewAverage", reviewAverage);
 				request.getRequestDispatcher("jsp/Top.jsp").forward(request, response);
 				return;
 			}
